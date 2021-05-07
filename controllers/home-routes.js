b1a99443ce4baf1ff44adc6795f35cc58fa02edf
Nick Verneck <nicollas@gmail.com>
2021-05-07 14:09:36 -0400

    added CPU.handlebars with top-10 and most popular ones

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User,CPU } = require('../models');
+const { Op } = require("sequelize");
 // homepage.handlebars
 router.get('/', (req, res) => {
   res.render('homepage')
@@ -11,6 +12,42 @@ router.get('/register', (req, res) => {
 // login.handlebars
 router.get('/login', (req, res) => {
   res.render('login');
+});
+// cpu.handlebars
+router.get('/cpu', async(req, res) => {
+  try {
+    const data = await CPU.findAll({
+      where:{
+        rank:{
+          [Op.between]:[1,10]
+        }
+      }
+    });
+    const cpuList = data.map((cpu) =>
+    cpu.get({ plain: true })
+  );
+  const data2 = await CPU.findAll({
+    where:{
+      samples:{
+        [Op.between]:[300000,820000]
+      }
+    },
+    order: [['samples', 'DESC']]
+  });
+  const cpuPop = data2.map((cpu) =>
+ cpu.get({ plain: true })
+);
+  
+
+
+    res.render('cpu',{cpuList,cpuPop});
+    
+    }
+    catch (err) {
+      console.log(err);
+      console.log("errrrow")
+    }
+  
 });
 
 
