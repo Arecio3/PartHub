20b86002b0eec21f314f39e4de50f2479306a657
Arecio Canton <cuba289@gmail.com>
2021-05-07 12:24:45 -0400

    added get gpu rank route

--- controllers/api/gpu-routes.js
+++ controllers/api/gpu-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Gpu } = require('../../models');
+const { Op } = require("sequelize");
 
 router.get('/', async (req, res) => {
     try {
@@ -13,11 +14,28 @@ router.get('/', async (req, res) => {
   });
   
   // GET one CPU by its ID
-  router.get('/gpu/:id', async (req, res) => {
+  router.get('/:id', async (req, res) => {
     try {
       const data = await Gpu.findByPk(req.params.id);
       res.status(200).json(data);
      } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/rank/:rank', async (req, res) => {
+    try {
+      const data = await Gpu.findAll({
+        where:{
+          rank:{
+            [Op.between]:[1,req.params.rank]
+          }
+        }
+      });
+     res.status(200).json(data);
+      }
+     catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
